a3f594c7593fd8e5fd5e04ee6343c92277360973
JOSE MARTIN PARDO MORALES <72662638>
2026-04-24 15:19:19 -0500

    update0319240426

--- src/import/import.controller.ts
+++ src/import/import.controller.ts
@@ -1,5 +1,5 @@
 // src/import/import.controller.ts
-import { Controller, Post, UploadedFile, UseInterceptors, Headers } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors, Headers, BadRequestException } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ImportService } from './import.service';
 
@@ -8,11 +8,18 @@ export class ImportController {
   constructor(private readonly importService: ImportService) {}
 
   @Post('excel')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file', {
+    limits: {
+      fileSize: 30 * 1024 * 1024, // 30 MB
+    },
+  }))
   async uploadExcel(
     @UploadedFile() file: Express.Multer.File,
     @Headers('x-user-id') adminId: string,
   ) {
+    if (!file) {
+      throw new BadRequestException('No se recibió el archivo. Verifica que la Key en Postman sea "file" y de tipo File.');
+    }
     return this.importService.importFromExcel(file.buffer, adminId);
   }
 }

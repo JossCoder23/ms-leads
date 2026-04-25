28b783a0c8aa18a6db0a985e355a90fcbe2624d9
Jose Pardo <jose.pardo@upch.pe>
2026-04-25 15:30:23 -0500

    update1530250426

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   console.log('🔍 DATABASE_URL actual:', process.env.DATABASE_URL);
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
